823c52ef335dc57e743e971fb65bd9231bb1f802
Nandeesh2222 <102343556+Nandeesh2222@users.noreply.github.com>
2023-04-15 22:13:49 +0530

    mj

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
-const API_KEY ='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'
+const API_KEY ="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"
 const submitButton = document.querySelector('#submit')
+const outPutElement = document.querySelector('#outpuut')
+const inputElement = document.querySelector('input')
+const historyElement = document.querySelector('.history')
+const buttonElement = document.querySelector('button')
+
+function changeInput(value){
+    const inputElement = document.querySelector('input')
+}
 async function getMessage(){
     console.log('clicked')
-    const options = {
+    const op = {
         method: 'POST',
-        headers: {'Authorization':'Bearer ${API_KEY',
+        headers: {'Authorization':'Bearer $OPENAI_API_KEYY',
                   'Content-Type':'application/json'},
-                  body: JSON.stringify({
-                    model:"gpt-3.5-turbo",
-                    messeges:[{role:"user" , content:"Hello!"}],
-                    max_tokens: 100,
+     body: JSON.stringify({
+                    
+         model:"gpt-3.5-turbo",
+        messeages:[{role:"user",content:inputElement.value}],
+          max_tokens:100,
                   })
                   }
    
     try {
-        const response = await fetch('https://api.openai.com/v1/completions',options)
+        const response = await fetch("https://api.openai.com/v1/completions",op)
         const data = await response.json()
         console.log(data)
+        outPutElement.textContent= data.choices[0].messeage.content
+        if(data.choices.messeage.content && inputElement.value){
+            const pElement = document.createElement('p')
+            pElement.textContent=inputElement.value
+            pElement.addEventListener('click',()=> changeInput(pElement.textContent))
+            historyElement.append(pElement)
+        }
     }
     catch (error){
         console.error(error)
@@ -24,3 +40,7 @@ async function getMessage(){
 }
 
 submitButton.addEventListener('click', getMessage)
+function clearInput(){
+    inputElement.value=""
+}
+buttonElement.addEventListener('cllick', clearInput)
